7aa8f058c65750ae142ffd56bcd09422461347e0
Saketram Durbha <saketramd@google.com>
2020-08-22 21:18:36 -0400

    add basic profiles component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfilesComponent } from './profiles/profiles.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'profiles', component: ProfilesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
